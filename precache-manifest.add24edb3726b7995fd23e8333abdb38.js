4189ba3e3aa92e585a31a34ffce96654db3744fc
satomo <satriyo55@ymail.com>
2018-12-28 18:24:22 +0700

    Updates

---
+++ precache-manifest.add24edb3726b7995fd23e8333abdb38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb49cc1f15462c03a923",
+    "url": "/bambu-chart/static/js/runtime~main.eb49cc1f.js"
+  },
+  {
+    "revision": "56c1eb6d151c64f82c9e",
+    "url": "/bambu-chart/static/js/main.56c1eb6d.chunk.js"
+  },
+  {
+    "revision": "e7f43aa06df8e37a56e9",
+    "url": "/bambu-chart/static/js/1.e7f43aa0.chunk.js"
+  },
+  {
+    "revision": "56c1eb6d151c64f82c9e",
+    "url": "/bambu-chart/static/css/main.42a1ab20.chunk.css"
+  },
+  {
+    "revision": "70ef14a2537489aac37431c39f780f8e",
+    "url": "/bambu-chart/index.html"
+  }
+];
